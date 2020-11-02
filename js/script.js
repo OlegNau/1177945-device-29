8c55da62c101a4ce182586d93bedc69c9fb3999f
OlegNau <70605440+OlegNau@users.noreply.github.com>
2020-11-02 13:36:28 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,15 @@ let creditButton = document.querySelector(".credit-button");
 let delivery = document.querySelector(".delivery");
 let guarantee = document.querySelector(".guarantee");
 let credit = document.querySelector(".credit");
-
+const modalWriteUs = document.querySelector(".modal-write-us");
+const writeButton = document.querySelector(".write-button");
+const writeUsClose = document.querySelector(".write-us-close");
+let message = document.querySelector(".message");
+let inputName = document.querySelector(".input-name");
+let inputEmail = document.querySelector(".input-email");
+const mapButton = document.querySelector(".map-button");
+const mapModal = document.querySelector(".map-modal");
+const mapClose = document.querySelector(".map-close");
 
 
 catalogListButton.addEventListener("click", function(evt) {
@@ -78,4 +86,43 @@ creditButton.addEventListener("click", function(evt) {
   creditButton.classList.add("services-button-active");
   guaranteeButton.classList.remove("services-button-active");
   deliveryButton.classList.remove("services-button-active");
+});
+
+writeButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalWriteUs.classList.remove("write-us-hide");
+  inputName.focus();
+});
+
+writeUsClose.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalWriteUs.classList.add("write-us-hide");
+});
+
+modalWriteUs.addEventListener("submit",function (evt) {
+  if(!inputName.value) {
+  evt.preventDefault();
+  inputName.classList.add("input-error-style");}
+});
+
+modalWriteUs.addEventListener("submit",function (evt) {
+  if(!message.value) {
+  evt.preventDefault();
+  message.classList.add("input-error-style");}
+});
+
+modalWriteUs.addEventListener("submit",function (evt) {
+  if(!inputEmail.value) {
+  evt.preventDefault();
+  inputEmail.classList.add("input-error-style");}
+});
+
+mapButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mapModal.classList.remove("map-modal-hide");
+});
+
+mapClose.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mapModal.classList.add("map-modal-hide");
 });
